5eff451d1ce319220c957387e840de30a342e26a
Hampus Farner <hampusfarner@outlook.com>
2024-09-03 10:40:03 +0200

    Add Game, Card, and Grid component

---
+++ src/components/Game.jsx
@@ -0,0 +1,10 @@
+import Grid from './Grid';
+import Card from './Card';
+
+export default function Game() {
+  return (
+    <>
+      <Grid />
+    </>
+  );
+}
---
+++ src/components/Grid.jsx
@@ -0,0 +1,5 @@
+import Card from './Card';
+
+export default function Grid() {
+  return <div className='gamegrid'>asd</div>;
+}
